10fb966d4979f45fcb421b5e63b307ac7ce513af
mende273 <mende_273@hotmail.com>
2024-07-10 23:32:22 +0200

    Initial commit

---
+++ app/src/main/java/mende273/apollographqlclientexample/MainActivity.kt
@@ -0,0 +1,49 @@
+package mende273.apollographqlclientexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mende273.apollographqlclientexample.ui.theme.ApolloGraphqlClientExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ApolloGraphqlClientExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ApolloGraphqlClientExampleTheme {
+        Greeting("Android")
+    }
+}
+
+// https://spacex-production.up.railway.app/
