8cc9a0dc7de5a33937f47b36c59bbe3d64599e3c
mende273 <mende_273@hotmail.com>
2024-07-11 15:45:15 +0200

    create domain models

---
+++ app/src/main/java/mende273/apollographqlclientexample/domain/model/Rocket.kt
@@ -0,0 +1,9 @@
+package mende273.apollographqlclientexample.domain.model
+
+data class Rocket(
+    val name: String,
+    val company: String,
+    val country: String,
+    val description: String,
+    val wikipedia: String,
+)
