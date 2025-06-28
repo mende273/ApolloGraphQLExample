cabbbe2829d8a4bf2683d58bc315479dd7dc2a78
mende273 <mende_273@hotmail.com>
2025-06-28 02:33:56 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "mende273.apollographqlclientexample"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "mende273.apollographqlclientexample"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
